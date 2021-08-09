bddf4470ff0a7533ad276805a168e988a126a47c
menooa25 <58942186+menooa25@users.noreply.github.com>
2021-08-10 00:34:32 +0430

    blueprints deleted and todo list for modeling added

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_restful import Api
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('config.py')
+    app.secret_key = 'z0=@^1&nb@67ssv1)u9%(&sz@f%6u$*69d1xpswp@50euzcmp_'
+    api = Api(app)
 
     @app.route('/')
     def hello_world():
---
+++ models.py
@@ -0,0 +1,6 @@
+# todo: mr. gachpazha ==> User
+
+# todo: mr. noori with 25% of mr. jafari help ==> Post
+
+
+# todo: mr. jafari ==> category
