58d9f80970e0b4ac524ea5a748f16fd352737c27
ali <ali.nouri213@gmail.com>
2021-08-10 18:32:35 +0430

    add post modles

--- models.py
+++ models.py
@@ -1,6 +1,27 @@
+from mongoengine import connect, Document, StringField, ReferenceField, ListField, CASCADE, EmbeddedDocument, EnumField, EmbeddedDocumentField
 # todo: mr. gachpazha ==> User
 
 # todo: mr. noori with 25% of mr. jafari help ==> Post
 
+
+class Comment(EmbeddedDocument):
+    content = StringField(required=True)
+    user = StringField()
+
+
+class Like(EmbeddedDocument):
+    user = StringField()
+
+
+class Dislike(EmbeddedDocument):
+    user = StringField()
+
+
+class Post(Document):
+    title = StringField(max_length=50, required=True)
+    description = StringField(required=True)
+    image = StringField()
+    tags = ListField(StringField(required=True))
+    comments = ListField(EmbeddedDocumentField(Comment))
 
 # todo: mr. jafari ==> category
