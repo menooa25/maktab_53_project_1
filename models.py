a5b939f97704f28885884ab949a7cfae31ffcde9
masoud00jfr <masood.jaafary@gmail.com>
2021-08-10 23:02:32 +0430

    add category

--- models.py
+++ models.py
@@ -1,6 +1,11 @@
+from mongoengine import *
 # todo: mr. gachpazha ==> User
 
 # todo: mr. noori with 25% of mr. jafari help ==> Post
 
 
 # todo: mr. jafari ==> category
+class Category(Document):
+    name = StringField(max_length=100, unique=True, required=True)
+    sub_cat = ReferenceField('self', required=False, reverse_delete_rule=CASCADE)
+    number_of_posts = IntField(min_value=0, default=0)
