d57c4d8fa682f7e43bd00fdd26dd52d0dc42e4c4
AliGachpazha <gachpazha2001@gmail.com>
2021-08-10 19:22:26 +0430

    change

--- models.py
+++ models.py
@@ -1,5 +1,14 @@
-# todo: mr. gachpazha ==> User
+from mongoengine import *
 
+
+class User(Document):
+    username = StringField(unique=True,required=True)
+    password = StringField(unique=True,required=True)
+    first_name = StringField(max_length=50,unique=True,required=True)
+    last_name = StringField(max_length=50,unique=True,required=True)
+    email = EmailField()
+    phone = StringField(unique=True,required=True)
+    image = ImageField(upload='media/images')
 # todo: mr. noori with 25% of mr. jafari help ==> Post
 
 
