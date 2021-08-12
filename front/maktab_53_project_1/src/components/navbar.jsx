f5e6dd1822a0eec33eb3888fc3c701d902d46155
menooa25 <58942186+menooa25@users.noreply.github.com>
2021-08-13 03:04:35 +0430

    front initialized

---
+++ front/maktab_53_project_1/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div className='container'>
+                <div className="row flex-row justify-content-between p-3">
+                    <div className=''>Hi</div>
+                    <div className=''>Hi</div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Navbar;
