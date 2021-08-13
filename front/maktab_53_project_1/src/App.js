f36c416acfb21a22256c78bd0e2b872b0e4c49d4
masoud00jfr <masood.jaafary@gmail.com>
2021-08-13 15:41:06 +0430

    add React Register

---
+++ front/maktab_53_project_1/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/navbar";
+import Register from "./components/account/register";
+
+function App() {
+  return (
+      <>
+      <Navbar />
+    <Register/>
+          </>
+  );
+}
+
+export default App;
