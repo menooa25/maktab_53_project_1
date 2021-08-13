1c2002a490f32064751d1f8a9b855147aa15da54
ali noori <ali.nouri213@gmail.com>
2021-08-13 13:41:14 +0430

    change init file

--- __init__.py
+++ __init__.py
@@ -1,14 +1,28 @@
+from datetime import timedelta
+
 from flask import Flask
-from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from .resources.user import RegisterUser
+from flask_restful import Api
+from flask_cors import CORS
+from .blacklist import BLACKLIST
+
+from .resources.user import RegisterUser, UserLogout,LoginUser
 
 
 def create_app():
     app = Flask(__name__)
+    app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+    app.config['JWT_BLACKLIST_ENABLED'] = True
     app.secret_key = 'z0=@^1&nb@67ssv1)u9%(&sz@f%6u$*69d1xpswp@50euzcmp_'
     api = Api(app)
+    cors = CORS(app)
     jwt = JWTManager(app)
 
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blacklist(jwt_header,decrypted_token):
+        return decrypted_token['jti'] in BLACKLIST
+
     api.add_resource(RegisterUser, '/register_user')
+    api.add_resource(LoginUser, '/login_user')
+    api.add_resource(UserLogout, '/logout')
     return app
