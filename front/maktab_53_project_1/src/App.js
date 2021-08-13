241f01f68b2b60ccfbe5b5aae309308503692e3a
menooa25 <58942186+menooa25@users.noreply.github.com>
2021-08-13 16:01:25 +0430

    profile component created

--- front/maktab_53_project_1/src/App.js
+++ front/maktab_53_project_1/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Navbar from "./components/navbar";
 import Login from "./components/account/login";
-
+import Profile from "./components/account/profile";
 function App() {
   return (
     <>
       <Navbar />
       <Login />
+      <Profile />
     </>
   );
 }
