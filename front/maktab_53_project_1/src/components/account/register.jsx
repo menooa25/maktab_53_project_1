a45c3d476e9a357df07af25acaa940052bfda5a8
masoud00jfr <masood.jaafary@gmail.com>
2021-08-13 15:49:23 +0430

    add form to register.jsx

--- front/maktab_53_project_1/src/components/account/register.jsx
+++ front/maktab_53_project_1/src/components/account/register.jsx
@@ -1,13 +1,146 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class Register extends Component {
-  render() {
-    return (
-      <>
+    render() {
+        return (
+            <>
+                <div className="card bg-light">
+                    <form
+                        action="http://127.0.0.1:5000/register_user"
+                        method="POST"
+                        encType="multipart/form-data"
+                    >
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                      <i className="fa fa-user"></i>{" "}
+                  </span>
+                            </div>
+                            <input
+                                name="username"
+                                className="form-control"
+                                placeholder="Username"
+                                type="text"
+                            />
+                        </div>
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                      <i className="fa fa-user"></i>{" "}
+                  </span>
+                            </div>
+                            <input
+                                name="first_name"
+                                className="form-control"
+                                placeholder="First name"
+                                type="text"
+                            />
+                        </div>
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                      <i className="fa fa-user"></i>{" "}
+                  </span>
+                            </div>
+                            <input
+                                name="last_name"
+                                className="form-control"
+                                placeholder="Last name"
+                                type="text"
+                            />
+                        </div>
 
-      </>
-    );
-  }
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                      <i className="fa fa-envelope"></i>{" "}
+                  </span>
+                            </div>
+                            <input
+                                name="email"
+                                className="form-control"
+                                placeholder="Email address"
+                                type="email"
+                            />
+                        </div>
+
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                      <i className="fa fa-phone"></i>{" "}
+                  </span>
+                            </div>
+                            <input
+                                name="phone"
+                                className="form-control"
+                                placeholder="Phone number"
+                                type="text"
+                            />
+                        </div>
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                      <i className="fa fa-image"></i>{" "}
+                  </span>
+                            </div>
+                            <input name="image" className="form-control" type="file"/>
+                        </div>
+
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                      <i className="fa fa-lock"></i>{" "}
+                  </span>
+                            </div>
+                            <input
+                                name="password"
+                                className="form-control"
+                                placeholder="Create password"
+                                type="password"
+                            />
+                        </div>
+
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                      <i className="fa fa-lock"></i>{" "}
+                  </span>
+                            </div>
+                            <input
+                                className="form-control"
+                                placeholder="Repeat password"
+                                type="password"
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <button
+                                onClick={(e) => this.handelOnSubmit(e)}
+                                type="submit"
+                                className="btn btn-primary btn-block"
+                            >
+                                {" "}
+                                Create Account
+                            </button>
+                        </div>
+
+                        <p className="text-center">
+                            Have an account? <a href="">Log In</a>
+                        </p>
+                    </form>
+                </div>
+
+            </>
+        );
+    }
 }
 
 export default Register;
