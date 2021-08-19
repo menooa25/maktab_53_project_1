e7b060eb7f61ad90872a331b2d834f477adb1e78
AliGachpazha <gachpazha2001@gmail.com>
2021-08-19 11:56:14 +0430

    add login_user1

---
+++ resources/post.py
@@ -0,0 +1,23 @@
+from flask_cors import cross_origin
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_restful import Resource, request
+from uuid import uuid4
+
+from ..models import User, Post as PostModel
+
+
+class Post(Resource):
+    @jwt_required()
+    @cross_origin()
+    def post(self):
+        request_data = {**request.form}
+        request_data['tags'] = request_data.get('tags').split(',')
+        the_post = PostModel(**request_data)
+        user = User.objects.get(id=get_jwt_identity())
+        the_post.user = user
+        if request.files.get('image'):
+            image = str(uuid4())
+            the_post.image = image
+            request.files.get('image').save(f'media/posts/{image}')
+        the_post.save()
+        return {'message': 'Post created successfully'}, 200
