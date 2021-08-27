200769ecb9fe6ce234d210d66b9fd2bd180b7002
AliGachpazha <gachpazha2001@gmail.com>
2021-08-27 17:55:02 +0430

    12

--- front/front/maktab_53_project_1/src/components/navbar.jsx
+++ front/front/maktab_53_project_1/src/components/navbar.jsx
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row flex-row justify-content-between p-3">
-          <div className="">Register</div>
-          <div className=""></div>
+      <div className="container-fluid bg-light">
+        <div className="container">
+          <div className="row flex-row justify-content-between p-3">
+            <div className="navbar">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+              {!sessionStorage.getItem("token") && (
+                <>
+                  <Link className="nav-link" to="/register">
+                    Register
+                  </Link>
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </>
+              )}
+              {sessionStorage.getItem("token") && (
+                <>
+                  <Link className="nav-link" to="/profile">
+                    Profile
+                  </Link>
+                  <Link className="nav-link" to="/logout">
+                    Logout
+                  </Link>
+                </>
+              )}
+            </div>
+            <div className="navbar">
+              <Link className="text-decoration-none text-dark" to="/">
+                <h3>WebsiteName</h3>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     );
